6f6cfb13167072ea0582fcae91bfc575651a2939
Marcos Alexandre <marcosalexandre100@gmail.com>
2021-10-22 19:25:19 -0300

    atualização buscaCep

--- js/buscarCep.js
+++ js/buscarCep.js
@@ -1,34 +1,62 @@
 
-const limparFormulario = (endereco) =>{
-    document.getElementById('endereco').value = '';
-    document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
-    document.getElementById('uf').value = '';
+function buscarCep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('endereco').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
 }
 
-
-const preencherFormulario = (endereco) =>{
-    document.getElementById('endereco').value = endereco.logradouro;
-    document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endereco.localidade;
-    document.getElementById('uf').value = endereco.uf;
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('endereco').value=(conteudo.logradouro);
+        document.getElementById('bairro').value=(conteudo.bairro);
+        document.getElementById('cidade').value=(conteudo.localidade);
+        document.getElementById('uf').value=(conteudo.uf);
+    } else {
+        //CEP não Encontrado.
+        buscarCep();
+        alert("CEP não encontrado.");
+    }
 }
 
+// monitora quando o elemento sofre mudança e executa a função
+document.getElementById('cep').onchange = function pesquisacep() {
+    var valor = document.getElementById('cep').value;
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
 
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
 
-const pesquisarCep = async() => {    
-    limparFormulario();
-    
-    const cep = document.getElementById('cep').value;  
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
-    const dados = await fetch(url);   
-    const endereco = await dados.json();  // pegamos o resultado e fazendo uma função json 
-        if (endereco.hasOwnProperty('erro')){
-            document.getElementById('endereco').value = 'CEP não encontrado!';
-        }else {
-            preencherFormulario(endereco); 
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('endereco').value="...";
+            document.getElementById('bairro').value="...";
+            document.getElementById('cidade').value="...";
+            document.getElementById('uf').value="...";
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+        } else {
+            //cep é inválido.
+            buscarCep();
+            alert("Formato de CEP inválido.");
         }
+    } else {
+        //cep sem valor, limpa formulário.
+        buscarCep();
+    }
 }
-
-document.getElementById('cep')   
-        .addEventListener('focusout',pesquisarCep);  //1º Passo, pega o cep digitado e faça a pesquisar quando sair do focus//
